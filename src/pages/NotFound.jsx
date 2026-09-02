d479c4f32fa4b537235b0620876ca38e776d1dd9
irislobato <irisblobato@gmail.com>
2026-09-02 13:17:45 -0300

    erro.css

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./NotFound.css"
 
 export default function NotFound() {
   return (
-      <div>
-          <h1>Error 404</h1>
-          <p>Esta página não existe mais.</p>
-          <Link to="/">Voltar para Home</Link>
+      <div className='erro-container'>
+          <h1 className='erro-titulo'>Error 404</h1>
+          <p className='erro-descricao'>Esta página não existe mais.</p>
+          <Link className='link-erro-home' to="/">Voltar para Home</Link>
     </div>
   )
 }
