fe5a28ad1de88ea5c56fbe7c45505e95a2874e51
irislobato <irisblobato@gmail.com>
2026-09-02 13:07:42 -0300

    css detalhesnoticias

--- src/pages/DetalhesNoticia.jsx
+++ src/pages/DetalhesNoticia.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { noticias } from "../data/noticia";
-import "./DetalhesNoticia.css"
+import "./DetalhesNoticia.css";
 
 export default function DetalhesNoticia() {
   const { id } = useParams();
-  
+
   const noticia = noticias.find((n) => n.id === Number(id));
 
   if (!noticia) {
@@ -13,14 +13,16 @@ export default function DetalhesNoticia() {
   }
 
   return (
-    <div>
-      <h1>Notícias</h1>
-      <img src={noticia.foto} alt={noticia.titulo} />
-      <p>{noticia.titulo}</p>
-      <p>{noticia.tema}</p>
-      <p>{noticia.textoCompleto}</p>
-      <p>{noticia.data}</p>
-      <p>{noticia.autor}</p>
+    <div className="container-detalhes">
+      <h1 className="titulo-detalhes">Notícias</h1>
+      <p className="tema-detalhes">{noticia.temas}</p>
+      <p className="titulo-noticias-detalhes">{noticia.titulo}</p>
+      <img className="img-detalhes" src={noticia.foto} alt={noticia.titulo} />
+      <p className="texto-detalhes">{noticia.textoCompleto}</p>
+      <div className="container-autor-data">
+        <p className="data-detalhes">{noticia.data}</p>
+        <p className="autor-detalhes">{noticia.autor}</p>
+      </div>
     </div>
   );
 }
